d1080dddc6ea16f57031b92b54a608cb61869546
vikashReactDev <baghelvikash0330@gmail.com>
2025-07-30 22:53:53 +0530

    git

---
+++ L2T2.js
@@ -0,0 +1,5 @@
+console.log("Session 2 Task 2");
+document.getElementById("first").innerText = "This is the first paragraph";
+document.getElementById("second").innerText = "This is the Second paragraph";
+document.getElementById("third").innerHTML = "<b>This is the third paragraph</b>";
+document.getElementById("Fourth").innerHTML = "<i> THis is the Fourth Paragraph </i>";
